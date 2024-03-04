bfd439a63b3bb756ccedce59924849ffac9ec797
Ming-Zhao Shi <shimzsy@gmail.com>
2024-03-04 16:17:58 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          My first todo list app
         </a>
       </header>
     </div>
